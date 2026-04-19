a0c12922a5facfa8469f71cb9f497b3b1d7f43b9
Krzysztof Tomaszek <s14937@pjwstk.edu.pl>
2026-04-19 19:01:26 +0200

    Uzupełniłem klasę Equipment

--- EquipRent/EquipRent/Models/Equipment.cs
+++ EquipRent/EquipRent/Models/Equipment.cs
@@ -3,5 +3,19 @@
 public class Equipment
 {
     private static int idToAssign = 1;
-    
+    private static List<Equipment> inventory = new List<Equipment>();
+
+    private Equipment(string make, string model)
+    {
+        this.Make = make;
+        this.Model = model;
+        Available = true;
+        Id = idToAssign++;
+        inventory.Add(this);
+    }
+
+    public string Make { get; }
+    public string Model { get; }
+    public bool Available { get; set; }
+    public int Id { get; }
 }
